544e21d5244fc3d55e8bad32cbfedfcadac9758f
Richard Kawala <rkawala@gmail.com>
2023-07-24 13:22:02 -0700

    Completed secrets exercise.

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,29 +1,29 @@
 defmodule Secrets do
   def secret_add(secret) do
-    # Please implement the secret_add/1 function
+    &(&1 + secret)  # I wanted to try the shorthand syntax.
   end
 
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    fn data -> data - secret end
   end
 
   def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
+    fn data -> data * secret end
   end
 
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    fn data -> div(data, secret) end
   end
 
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    &(Bitwise.band(&1, secret))
   end
 
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    fn data -> Bitwise.bxor(data, secret) end
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    fn data -> secret_function2.(secret_function1.(data)) end  # Note the dots. Dot is the "invoke function" operator.
   end
 end
