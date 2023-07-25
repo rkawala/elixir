0c50396fcd52d69b63a74f3ceafe7a129870fa1c
Richard Kawala <rkawala@gmail.com>
2023-07-24 17:02:58 -0700

    Completed freelancer-rates exercise.

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,20 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * ((100 - discount) / 100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    ceil(apply_discount(daily_rate(hourly_rate) * 22, discount))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    Float.floor(budget / apply_discount(daily_rate(hourly_rate), discount), 1)
   end
+
+  # TODO: Figure out why I need to put "Float." in front of "floor" but "ceil" doesn't need it.
+  # I think it has something to do with ceil's arity being 1, and floor's arity being 2.
 end
