2e2174f0d9559c034301527d8423205c44a5cdeb
Richard Kawala <rkawala@gmail.com>
2023-07-26 09:42:53 -0700

    DRY my code.
    I submitted my guessing-game code to Exercism, and the analysis suggested
    a default argument. That resulted in me learning about bodyless functions,
    AKA function headers. The code is DRYer now. Cool.
    
    I believe that bodyless functions are going to be taught in a future exercise
    that I haven't yet unlocked. I got enough of the idea for now to DRY my code.

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,19 +1,22 @@
 defmodule GuessingGame do
-  def compare(_secret_number) do
-    "Make a guess"
-  end
+  def compare(_secret_number, guess \\ :no_guess)
+
   def compare(_secret_number, :no_guess) do
     "Make a guess"
   end
+
   def compare(secret_number, guess) when guess == secret_number do
     "Correct"
   end
+
   def compare(secret_number, guess) when abs(guess - secret_number) == 1 do
     "So close"
   end
+
   def compare(secret_number, guess) when guess - secret_number > 1 do
     "Too high"
   end
+
   def compare(secret_number, guess) when guess - secret_number < -1 do
     "Too low"
   end
