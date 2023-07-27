65341405a72d50e63d77cfa17f93b81c8f88ab64
Richard Kawala <rkawala@gmail.com>
2023-07-26 18:57:55 -0700

    Completed kitchen-calculator exercise.

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,17 +1,34 @@
 defmodule KitchenCalculator do
   def get_volume(volume_pair) do
-    # Please implement the get_volume/1 function
+    elem(volume_pair, 1)
   end
 
   def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
+    { :milliliter, get_volume(volume_pair) * lookup_conversion_factor(volume_pair) }
   end
 
   def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
+    { unit, get_volume(volume_pair) / lookup_conversion_factor(unit) }
   end
 
   def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+    from_milliliter(to_milliliter(volume_pair), unit)
+  end
+
+  defp get_conversions() do
+    %{milliliter: 1, teaspoon: 5, tablespoon: 15, fluid_ounce: 30, cup: 240}
+  end
+
+  defp lookup_conversion_factor(key) when is_atom(key) do
+    get_conversions()[key]
+  end
+
+  defp lookup_conversion_factor(volume_pair) do
+    key = get_unit(volume_pair)
+    lookup_conversion_factor(key)
+  end
+
+  defp get_unit(volume_pair) do
+    elem(volume_pair, 0)
   end
 end
