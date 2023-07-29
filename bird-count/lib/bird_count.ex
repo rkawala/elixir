3468143b33b484d3c15ba5815b92e7135930cfbf
Richard Kawala <rkawala@gmail.com>
2023-07-28 19:13:27 -0700

    Completed bird-count exercise.

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,21 +1,34 @@
 defmodule BirdCount do
+  def today([]), do: nil
   def today(list) do
-    # Please implement the today/1 function
+    hd(list)
   end
 
+  def increment_day_count([]), do: [ 1 ]
   def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+    [ head | tail] = list
+    [ head + 1 | tail ]
   end
 
+  def has_day_without_birds?([]), do: false
   def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+    [ head | tail ] = list
+    head == 0 || has_day_without_birds?(tail)
   end
 
+  def total([]), do: 0
   def total(list) do
-    # Please implement the total/1 function
+    [ head | tail ] = list
+    head + total(tail)
   end
 
+  def busy_days([]), do: 0
   def busy_days(list) do
-    # Please implement the busy_days/1 function
+    [ head | tail ] = list
+    ternary(head >= 5, 1, 0) + busy_days(tail)
+  end
+
+  defp ternary(condition, if_true, if_false) do
+    if (condition), do: if_true, else: if_false
   end
 end
