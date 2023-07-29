b4509e7602601e051c2d98d53b38fd024c08521c
Richard Kawala <rkawala@gmail.com>
2023-07-29 08:57:47 -0700

    Completed high-score exercise.

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,25 +1,27 @@
 defmodule HighScore do
+  import Map
+
   def new() do
-    # Please implement the new/0 function
+    %{}
   end
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def add_player(scores, name, score \\ 0) do
+    put(scores, name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    put(scores, name, 0)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    update(scores, name, score, &(&1 + score))
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    keys(scores)
   end
 end
