e4b96bc22b4dff80009c896f0d9c1d69bcc163ec
Richard Kawala <rkawala@gmail.com>
2023-07-28 19:19:59 -0700

    I can shorten my code by putting [ head, tail ] as the argument.

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -5,26 +5,22 @@ defmodule BirdCount do
   end
 
   def increment_day_count([]), do: [ 1 ]
-  def increment_day_count(list) do
-    [ head | tail] = list
+  def increment_day_count([ head | tail]) do
     [ head + 1 | tail ]
   end
 
   def has_day_without_birds?([]), do: false
-  def has_day_without_birds?(list) do
-    [ head | tail ] = list
+  def has_day_without_birds?([ head | tail]) do
     head == 0 || has_day_without_birds?(tail)
   end
 
   def total([]), do: 0
-  def total(list) do
-    [ head | tail ] = list
+  def total([ head | tail]) do
     head + total(tail)
   end
 
   def busy_days([]), do: 0
-  def busy_days(list) do
-    [ head | tail ] = list
+  def busy_days([ head | tail]) do
     ternary(head >= 5, 1, 0) + busy_days(tail)
   end
 
