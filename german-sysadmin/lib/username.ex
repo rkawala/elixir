c54b47295259fe99396cfeae93a199b7a9f16262
Richard Kawala <rkawala@gmail.com>
2023-08-02 08:17:49 -0700

    german-sysadmin: I didn't need to calll sanitize() twice.
    Only one call was necessary. Actually, I have no idea why calling sanitize()
    twice didn't cause problems. But removing the unnecessary call makes code
    clearer. Also the tests run much faster, not that it was a problem before.

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -2,11 +2,11 @@ defmodule Username do
   def sanitize([]), do: []
   def sanitize([ head | tail ] = _username) do
     case head do
-      head when (head >= ?a and head <= ?z) or head == ?_ -> prepend(head, sanitize(tail))
-      head when head == ?ü -> prepend(?u, ?e, sanitize(tail))
-      head when head == ?ö -> prepend(?o, ?e, sanitize(tail))
-      head when head == ?ä -> prepend(?a, ?e, sanitize(tail))
-      head when head == ?ß -> prepend(?s, ?s, sanitize(tail))
+      head when (head >= ?a and head <= ?z) or head == ?_ -> prepend(head, tail)
+      head when head == ?ü -> prepend(?u, ?e, tail)
+      head when head == ?ö -> prepend(?o, ?e, tail)
+      head when head == ?ä -> prepend(?a, ?e, tail)
+      head when head == ?ß -> prepend(?s, ?s, tail)
       _ -> sanitize(tail)
     end
   end
