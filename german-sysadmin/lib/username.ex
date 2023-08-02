73e16f278d1fddb53965a906bc2e12d8a16097df
Richard Kawala <rkawala@gmail.com>
2023-08-02 02:52:07 -0700

    Completed german-sysadmin exercise.

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,10 +1,21 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  def sanitize([]), do: []
+  def sanitize([ head | tail ] = _username) do
+    case head do
+      head when (head >= ?a and head <= ?z) or head == ?_ -> prepend(head, sanitize(tail))
+      head when head == ?ü -> prepend(?u, ?e, sanitize(tail))
+      head when head == ?ö -> prepend(?o, ?e, sanitize(tail))
+      head when head == ?ä -> prepend(?a, ?e, sanitize(tail))
+      head when head == ?ß -> prepend(?s, ?s, sanitize(tail))
+      _ -> sanitize(tail)
+    end
+  end
 
-    # Please implement the sanitize/1 function
+  defp prepend(ch1, ch2, tail) do
+    prepend(ch1, prepend(ch2, tail))
+  end
+
+  defp prepend(ch, tail) do
+    [ ch | sanitize(tail) ]
   end
 end
