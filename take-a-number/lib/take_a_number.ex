641158bcf7dba17ecb6222331ebffa6b14109de5
Richard Kawala <rkawala@gmail.com>
2023-08-04 16:18:57 -0700

    Completed take-a-number exercise.

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,18 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> process(0) end)
+  end
+
+  defp process(last_number) do
+    Process.info(self(), :messages)
+    new_last_number = receive do
+      { :report_state, sender_pid } ->
+        send(sender_pid, last_number)
+      { :take_a_number, sender_pid } ->
+        send(sender_pid, last_number + 1)
+      :stop -> -1
+      _ -> last_number
+    end
+    if new_last_number >= 0, do: process(new_last_number)
   end
 end
