78c598b569cc87d3bccb52a6f09f2841fe7b7895
Richard Kawala <rkawala@gmail.com>
2023-08-15 08:46:16 -0700

    Completed paint-by-number exercise.

--- paint-by-number/lib/paint_by_number.ex
+++ paint-by-number/lib/paint_by_number.ex
@@ -1,29 +1,42 @@
 defmodule PaintByNumber do
-  def palette_bit_size(color_count) do
-    # Please implement the palette_bit_size/1 function
-  end
+  def palette_bit_size(color_count), do: count_palette_bits(color_count - 1)
 
   def empty_picture() do
-    # Please implement the empty_picture/0 function
+    <<>>
   end
 
   def test_picture() do
-    # Please implement the test_picture/0 function
+    <<0::2, 1::2, 2::2, 3::2>>
   end
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    <<pixel_color_index::size(palette_bit_size(color_count)), picture::bitstring>>
   end
 
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    hd(separate_first_and_rest(picture, color_count, nil))
   end
 
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    tl(separate_first_and_rest(picture, color_count, <<>>))
   end
 
   def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
+    <<picture1::bitstring, picture2::bitstring>>
+  end
+
+  defp count_palette_bits(0), do: 0
+  defp count_palette_bits(color_count) do
+    1 + count_palette_bits(Bitwise.bsr(color_count, 1))
+  end
+
+  defp separate_first_and_rest(picture, color_count, empty_element) do
+    if (picture == <<>>) do
+      [ empty_element | empty_element ]
+    else
+      pixel_size = palette_bit_size(color_count)
+      <<first_pixel::size(pixel_size), rest::bitstring>> = picture
+      [ first_pixel | rest ]
+    end
   end
 end
