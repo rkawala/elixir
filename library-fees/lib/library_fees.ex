bee3911fcfcbf198decc6970aa4559a69a2569c7
Richard Kawala <rkawala@gmail.com>
2023-08-18 16:17:15 -0700

    library-fees exercise completed.

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -1,25 +1,47 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    { :ok, result } = NaiveDateTime.from_iso8601(string)
+    result
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    { secs, _ } =
+      NaiveDateTime.to_time(datetime)
+      |> Time.to_seconds_after_midnight
+    secs < 12 * 3600
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    duration = case before_noon?(checkout_datetime) do
+      true -> 28
+      false -> 29
+    end
+    NaiveDateTime.add(checkout_datetime, duration, :day)
+    |> NaiveDateTime.to_date()
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    date_returned = NaiveDateTime.to_date(actual_return_datetime)
+    case Date.diff(date_returned, planned_return_date) do
+      x when x > 0 -> x
+      _ -> 0
+    end
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    day_of_week_returned = NaiveDateTime.to_date(datetime)
+      |> Date.day_of_week()
+    case day_of_week_returned do
+      1 -> true
+      _ -> false
+    end
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    due_date = return_date(datetime_from_string(checkout))
+    date_returned = datetime_from_string(return)
+    days_late = days_late(due_date, date_returned)
+    amount = days_late * rate
+    if monday?(date_returned) do div(amount, 2) else amount end
   end
 end
