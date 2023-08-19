492662b7b11f6a182818565a6b99c18670c465a8
Richard Kawala <rkawala@gmail.com>
2023-08-19 10:40:47 -0700

    Completed basketball-website exercise.

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,11 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    parts = String.split(path, ".")
+    Enum.reduce(parts, data, &(&2[&1]))
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    parts = String.split(path, ".")
+    get_in(data, parts)
   end
 end
