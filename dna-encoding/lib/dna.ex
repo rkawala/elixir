6e65064e89a6f5d21192e1c7934a56df38d78b01
Richard Kawala <rkawala@gmail.com>
2023-08-21 12:10:48 -0700

    Completed dna-encoding exercise.
    The instructions said to use tail recursion, so I did. But I wonder if
    tail recursion was the tool I would've used in production: to use it, I
    had to concatenate bitstrings and lists, which is expensive. It looks
    nice, though.

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -1,17 +1,39 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      ?\s -> 0
+      ?A -> 1
+      ?C -> 2
+      ?G -> 4
+      ?T -> 8
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0 -> ?\s
+      1 -> ?A
+      2 -> ?C
+      4 -> ?G
+      8 -> ?T
+    end
   end
 
   def encode(dna) do
-    # Please implement the encode/1 function
+    do_encode(dna, <<>>)
+  end
+
+  defp do_encode([], encoded_bitstring), do: encoded_bitstring
+  defp do_encode([ head | tail ], encoded_bitstring) do
+    do_encode(tail, <<encoded_bitstring::bitstring, encode_nucleotide(head)::size(4) >>)
   end
 
   def decode(dna) do
-    # Please implement the decode/1 function
+    do_decode(dna, [])
+  end
+
+  defp do_decode(<<>>, decoded_string), do: decoded_string
+  defp do_decode(<< nucleotide::size(4), rest::bitstring >>, decoded_string) do
+    do_decode(rest, decoded_string ++ [ decode_nucleotide(nucleotide) ])
   end
 end
