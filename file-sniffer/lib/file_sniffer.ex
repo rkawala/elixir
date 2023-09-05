c9dcd98cc20ae08b960cac727acf43845897b97e
Richard Kawala <rkawala@gmail.com>
2023-09-05 12:54:37 -0700

    Completed file-sniffer exercise.

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,44 @@
 defmodule FileSniffer do
+
+  @types [
+    %{ file_ext: "exe", signature: <<0x7F, 0x45, 0x4C, 0x46>>, desc: "application/octet-stream"},
+    %{ file_ext: "bmp", signature: <<0x42, 0x4D>>, desc: "image/bmp"},
+    %{ file_ext: "png", signature: <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>, desc: "image/png"},
+    %{ file_ext: "jpg", signature: <<0xFF, 0xD8, 0xFF>>, desc: "image/jpg"},
+    %{ file_ext: "gif", signature: <<0x47, 0x49, 0x46>>, desc: "image/gif"},
+  ]
+
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    type_from_lambda(&(&1.file_ext == extension))
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    file_size = byte_size(file_binary)
+    type_from_lambda(fn entry ->
+      signature_size = byte_size(entry.signature)
+      if file_size > signature_size do
+        << start::binary-size(signature_size), _rest::binary>> = file_binary
+        entry.signature == start
+      else
+        false
+      end
+      end)
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    from_ext = type_from_extension(extension)
+    from_sig = type_from_binary(file_binary)
+    cond do
+      from_ext && from_ext == from_sig -> { :ok, from_ext }
+      true -> { :error, "Warning, file format and file extension do not match." }
+    end
+  end
+
+  defp type_from_lambda(lambda) do
+    entry = Enum.find(@types, lambda)
+    cond do
+      entry -> entry.desc
+      true -> nil
+    end
   end
 end
